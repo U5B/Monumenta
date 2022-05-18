3c5ae509d0808ad9b7268face5d1de3ae00b18ba
usb <56985400+U5B@users.noreply.github.com>
2022-05-18 20:05:13 +0000

    wiki test

---
+++ advancements/poiConvertWiki.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const pois = require('./out/pois.json')
+let lines = ['==List of Known POIs ==',
+'{|class"article-table sortable"', '!Name', '!Region', '!Sub-Region',
+'! colspan="3" | Coordinates', '|-']
+
+for (const [name, poi] of Object.entries(pois)) {
+  const poiName = `|[[${poi.name}]]`
+  lines.push(poiName)
+  const regionName = `|[[${poi.shard}]]`
+  lines.push(regionName)
+  const subRegionName = `|${poi.region}`
+  lines.push(subRegionName)
+  if (poi.coordinates == null) {
+    poi.coordinates = {
+      x: 0,
+      y: 0,
+      z: 0
+    }
+  }
+  const x = `|<nowiki>${poi.coordinates.x}</nowiki>`
+  lines.push(x)
+  const y = `|<nowiki>${poi.coordinates.y}</nowiki>`
+  lines.push(y)
+  const z = `|<nowiki>${poi.coordinates.z}</nowiki>`
+  lines.push(z)
+  const seperator = `|-`
+  lines.push(seperator)
+}
+lines.pop()
+lines.push('|}')
+const output = lines.join('\n')
+fs.writeFileSync('./test.txt', output)
+
+
