42b6a897d0ecfc69e9c219190836c86eae27113a
U5B <56985400+U5B@users.noreply.github.com>
2023-02-21 20:08:09 -0800

    fixed the dungeon coordinate bug!

--- advancements/index.js
+++ advancements/index.js
@@ -8,10 +8,6 @@ const fs = require('fs')
 
 let advancements
 const regex = {
-  // r1 and r2 coordinates
-  coordinates: /^x=(-?\d{1,5}) y=(-?\d{1,5}) z=(-?\d{1,5})$/, // $1 = x, $2 = y, $3 = z
-  // r3 coordinates
-  coordinatesr3: /^(-?\d{1,5}) (-?\d{1,5}) (-?\d{1,5})$/, // $1 = x, $2 = y, $3 = z
   poi: {
     shard: /^monumenta:pois\/([0-9a-zA-Z-_.]+)\/root$/, // $1 = shard
     region: /^monumenta:pois\/([0-9a-zA-Z-_.]+)\/([0-9a-zA-Z-_.]+)\/root$/, // $1 = shard, $2 = region
@@ -22,17 +18,26 @@ const regex = {
   },
   dungeon: {
     path: /^monumenta:dungeons\/([0-9a-zA-Z-_.]+)\/find$/, // dungeon path
-    prefix: /^Found (.+)/ // ignore prefix 'Found' in dungeon titles
+    prefix: /^Found(?: the)? (.+)/, // ignore prefix 'Found' or 'Found the' in dungeon titles
+    poi: /POI: (.+) Coordinates.*/
   },
   quest: {
     path: /^monumenta:quests\/([0-9a-zA-Z-_.]+)\/((?!root)[0-9a-zA-Z-_.]+)$/, // $1 = region, $2 = quest && $2 != 'root'
-    city: /^Discover the (.+)$/, // ignore prefix 'Discover the' fromcities
-    ignore: /^(.+) Quests$/
+    city: /Discover the (.+)/, // ignore prefix 'Discover the' fromcities
+    ignore: /(.+) Quests/
   },
   handbook: {
     enchantments: /^monumenta:handbook\/enchantments\/([0-9a-zA-Z-_.]+)$/,
     ignore: /^monumenta:handbook\/enchantments\/root$/,
     sites: /^monumenta:handbook\/important_sites\/([0-9a-zA-Z-_.]+)\/([0-9a-zA-Z-_.]+)$/
+  },
+  description: {
+    // r1 and r2 coordinates
+    coordinates: /x=(-?\d{1,5}) y=(-?\d{1,5}) z=(-?\d{1,5})/, // $1 = x, $2 = y, $3 = z
+    // r3 coordinates
+    coordinatesr3: /(-?\d{1,5}) (-?\d{1,5}) (-?\d{1,5})/, // $1 = x, $2 = y, $3 = z
+    colorcodes: /&#\d+;/g,
+    newlines: / ?\n ?/g
   }
 }
 const converter = {
@@ -48,7 +53,7 @@ const enchantments = {}
 const all = {}
 
 async function fetchAdvancements () {
-  if (fs.existsSync('./advancement.json') && process.env.DEBUG === "true") {
+  if (fs.existsSync('./advancement.json') && process.env.DEBUG === 'true') {
     console.log('[FILE] Loading advancements from existing file...')
     advancements = require('./advancement.json')
     return
@@ -101,21 +106,24 @@ function generate () {
 function convertPoi (advancement) {
   const id = advancement.id
   const data = advancement?.display
-  let title = data.title.text
+  const title = data.title
+  if (!title) return
+
+  const parsedTitle = parseChatJson(title)
   switch (true) {
     // poi converter paths
     case regex.poi.shard.test(id): { // monumenta:pois/r1/root
       const [, region] = regex.poi.shard.exec(id)
       if (!converter.poi[region]) converter.poi[region] = {}
-      title = title.replace(regex.poi.suffix, '$1') // remove Exploration at end of string
-      converter.poi[region].name = title
+      const poiTitle = parsedTitle.replace(regex.poi.suffix, '$1') // remove Exploration at end of string
+      converter.poi[region].name = poiTitle
       break
     }
     case regex.poi.region.test(id): { // monumenta:pois/r1/jungle/root
       const [, region, subregion] = regex.poi.region.exec(id)
       if (!converter.poi[region]) converter.poi[region] = {}
       if (!converter.poi[region][subregion]) converter.poi[region][subregion] = {}
-      converter.poi[region][subregion].name = title
+      converter.poi[region][subregion].name = parsedTitle
       break
     }
     case regex.poi.subregion.test(id): { // monumenta:pois/r1/jungle/south/root
@@ -123,7 +131,7 @@ function convertPoi (advancement) {
       if (!converter.poi[region]) converter.poi[region] = {}
       if (!converter.poi[region][subregion]) converter.poi[region][subregion] = {}
       if (!converter.poi[region][subregion][subsubregion]) converter.poi[region][subregion][subsubregion] = {}
-      converter.poi[region][subregion][subsubregion].name = title
+      converter.poi[region][subregion][subsubregion].name = parsedTitle
       break
     }
   }
@@ -132,14 +140,7 @@ function convertPoi (advancement) {
 function convertQuest (advancement) {
   const id = advancement.id
   const data = advancement?.display
-  let title = data.title
-  let description = data.description
-  if (!title || !description) return
-  if (!Array.isArray(title)) title = [title]
-  if (!Array.isArray(description)) description = [description]
 
-  const titlePrint = parseChatJson(title)
-  const descriptionPrint = parseChatJson(description)
   if (!regex.quest.path.test(id)) return
   const [, region, quest] = regex.quest.path.exec(id)
   if (!regex.quest.path.test(advancement.parent)) return
@@ -150,11 +151,9 @@ function convertQuest (advancement) {
 function convertHandbook (advancement) {
   const id = advancement.id
   const data = advancement?.display
-  let title = data.title
-  let description = data.description
+  const title = data.title
+  const description = data.description
   if (!title || !description) return
-  if (!Array.isArray(title)) title = [title]
-  if (!Array.isArray(description)) description = [description]
 
   const titlePrint = parseChatJson(title)
   const descriptionPrint = parseChatJson(description)
@@ -190,17 +189,15 @@ function parseAny (advancement) {
 
   // null checking
   if (!display) return
-  let title = display.title
-  let description = display.description
+  const title = display.title
+  const description = display.description
   if (!title || !description) return
-  if (!Array.isArray(title)) title = [title] // titles can have multiple lines
-  if (!Array.isArray(description)) description = [description] // description can have multiple lines
-  title = parseChatJson(title)
-  description = parseChatJson(description)
+  const parsedTitle = parseChatJson(title)
+  const parsedDescription = parseChatJson(description)
 
   all[advancement.id] = {
-    title: title,
-    description: description
+    title: parsedTitle,
+    description: parsedDescription
   }
 }
 
@@ -210,11 +207,9 @@ function parseAdvancement (advancement, options = { type: '' }) {
 
   // null checking
   if (!display) return
-  let title = display.title
-  let description = display.description
+  const title = display.title
+  const description = display.description
   if (!title || !description) return
-  if (!Array.isArray(title)) title = [title] // titles can have multiple lines
-  if (!Array.isArray(description)) description = [description] // description can have multiple lines
   const option = { id, display, title, description, advancement }
   switch (options.type) {
     case 'poi': {
@@ -237,68 +232,64 @@ function parseAdvancement (advancement, options = { type: '' }) {
 }
 
 function parsePoi ({ id, display, title, description, advancement }) {
-  title = parseChatJson(title) // make title into one line
+  const parsedTitle = parseChatJson(title) // make title into one line
+  const parsedDescription = parseChatJson(description)
   // check if advancement has a valid path
   const longPoi = regex.poi.long.test(id)
   const shortPoi = regex.poi.short.test(id)
   if ((longPoi || shortPoi) === false) return
   // generate poi information
-  const data = { name: title.trim(), shard: '', region: '', subregion: '', coordinates: { x: '0', y: '0', z: '0' } }
+  const data = { name: parsedTitle.trim(), shard: '', region: '', subregion: '', coordinates: null }
   if (longPoi) { // monumenta:pois/r1/jungle/south/poi1
     const [, shard, region, subregion] = regex.poi.long.exec(id)
     data.shard = converter.poi[shard].name
     data.region = converter.poi[shard][region].name
     data.subregion = converter.poi[shard][region][subregion].name
-    data.coordinates = parseCoordinates(description[3]?.text)
+    data.coordinates = parseCoordinates(parsedDescription)
     // coordinates may be missing
-    if (!data.coordinates) console.error(`[POI] '${title}' missing coordinates | advancement: '${id}'`)
+    if (!data.coordinates) console.error(`[POI] '${parsedTitle}' missing coordinates | advancement: '${id}'`)
     if (!pois[data.name]) pois[data.name] = data // add data
-    else console.error(`[POI] '${title}' has duplicate name | advancement: '${id}'`)
+    else console.error(`[POI] '${parsedTitle}' has duplicate name | advancement: '${id}'`)
   } else if (shortPoi) { // monumenta:pois/r1/jungle/poi1
     const [, shard, region] = regex.poi.short.exec(id)
     data.shard = converter.poi[shard].name
     data.region = converter.poi[shard][region].name
     data.subregion = null
-    data.coordinates = parseCoordinates(description[3]?.text)
+    data.coordinates = parseCoordinates(parsedDescription)
     // coordinates may be missing
-    if (!data.coordinates) console.error(`[POI] '${title}' missing coordinates | advancement: '${id}'`)
+    if (!data.coordinates) console.error(`[POI] '${parsedTitle}' missing coordinates | advancement: '${id}'`)
     if (!pois[data.name]) pois[data.name] = data // add data
-    else console.error(`[POI] '${title}' has duplicate name | advancement: '${id}'`)
+    else console.error(`[POI] '${parsedTitle}' has duplicate name | advancement: '${id}'`)
   }
 }
 
 function parseDungeon ({ id, display, title, description, advancement }) {
-  title = parseChatJson(title) // make title into one line
+  let parsedTitle = parseChatJson(title) // make title into one line
+  const parsedDescription = parseChatJson(description)
   // pre checks
   if (!regex.dungeon.path.test(id)) return
 
   // generate dungeon information
-  title = title.replace(regex.dungeon.prefix, '$1') // get rid of 'Found ' prefix on dungeon name
-  const lines = {
-    1: cleanDescriptionLine(description[0]?.text), // description of dungeon
-    2: cleanDescriptionLine(description[1]?.text), // 'POI:' or 'Coordinates:'
-    3: cleanDescriptionLine(description[2]?.text) // POI name or coordinates data
-  }
-  const data = { name: title.trim(), description: lines[1], poi: '', coordinates: { x: '0', y: '0', z: '0' } }
-  if (lines[2] === 'POI:') data.poi = lines[3] // hardcoding POI data (not good)
-  else data.poi = null
-  for (const line of description) { // check for coordinates in a loop (good)
-    const text = cleanDescriptionLine(line.text)
-    if (text === '' || text == null) continue // skip empty lines
-    if (regex.coordinates.test(text)) data.coordinates = parseCoordinates(text) // coordinates easy to parse
-  }
+  parsedTitle = parsedTitle.replace(regex.dungeon.prefix, '$1') // get rid of 'Found ' prefix on dungeon name
+  const data = { name: parsedTitle.trim(), description: null, poi: '', coordinates: null }
+  data.description = parseChatJson(description[0])
+  data.poi = parsePois(parsedDescription)
+  if (!data.poi) console.error(`[DUNGEON] '${parsedTitle}' missing poi data | advancement: '${id}'`)
+  data.coordinates = parseCoordinates(parsedDescription)
+  if (!data.coordinates) console.error(`[DUNGEON] '${parsedTitle}' missing coordinates | advancement: '${id}'`)
   if (!dungeons[data.name]) dungeons[data.name] = data // add data
-  else console.error(`[DUNGEON] '${title} has duplicate name | advancement: '${id}'`)
+  else console.error(`[DUNGEON] '${parsedTitle} has duplicate name | advancement: '${id}'`)
 }
 
 function parseQuest ({ id, display, title, description, advancement }) {
-  title = parseChatJson(title) // make title into one line
+  const parsedTitle = parseChatJson(title) // make title into one line
+  const parsedDescription = parseChatJson(description)
   // pre checks
-  if (regex.quest.ignore.test(title.trim())) return // ignore `xxx Quests`
-  if (regex.quest.city.test(description[0]?.text)) return
+  if (regex.quest.ignore.test(parsedTitle.trim())) return // ignore `xxx Quests`
+  if (regex.quest.city.test(parsedDescription)) return
 
-  const data = { name: title.trim(), description: '', region: '', city: '' }
-  data.description = parseChatJson(description) // make description into one line
+  const data = { name: parsedTitle, description: '', region: '', city: '' }
+  data.description = parsedDescription // make description into one line
   if (regex.quest.path.test(id)) { // add region information
     const [, region, quest] = regex.quest.path.exec(id)
     data.region = converter.poi[region].name // use POI mapping
@@ -314,12 +305,12 @@ function parseQuest ({ id, display, title, description, advancement }) {
 }
 
 function parseHandbook ({ id, display, title, description, advancement }) {
-  title = parseChatJson(title)
-  description = parseChatJson(description)
+  const parsedTitle = parseChatJson(title)
+  const parsedDescription = parseChatJson(description)
   switch (true) {
     case regex.handbook.enchantments.test(id): {
       if (regex.handbook.ignore.test(id) && title !== 'Agility') break
-      const data = { name: title, description: description, category: '' }
+      const data = { name: parsedTitle, description: parsedDescription, category: '' }
       const [, enchantment] = regex.handbook.enchantments.exec(id)
       data.category = checkEnchantment(enchantment)
       enchantments[enchantment] = data
@@ -350,30 +341,38 @@ function checkEnchantment (name = '') { // recursively check for enchantments
   return value
 }
 
-function parseCoordinates (text = '') {
+function parseCoordinates (text) {
   if (!text) return null
-  if (regex.coordinates.test(text)) {
-    const [, x, y, z] = regex.coordinates.exec(text)
+  if (regex.description.coordinates.test(text)) {
+    const [, x, y, z] = regex.description.coordinates.exec(text)
+    return { x, y, z }
+  } else if (regex.description.coordinatesr3.test(text)) {
+    const [, x, y, z] = regex.description.coordinatesr3.exec(text)
     return { x, y, z }
   }
-  else if (regex.coordinatesr3.test(text)) {
-    const [, x, y, z] = regex.coordinatesr3.exec(text)
-    return { x, y, z }
+  return null
+}
+
+function parsePois (text) {
+  if (!text) return null
+  if (regex.dungeon.poi.test(text)) {
+    const [, poi] = regex.dungeon.poi.exec(text)
+    return poi
   }
+  return null
 }
 
 function parseChatJson (json = '') {
   if (!json) return null
   const msg = new ChatMessage(json)
-  const output = cleanDescriptionLine(msg.toString())
-  return output
+  return cleanDescriptionLine(msg.toString())
 }
 
 function cleanDescriptionLine (text = '') {
   if (!text) return null
   return String(text)
-    .replaceAll('\n', ' ') // clean new lines
-    .replaceAll(/&#\d+; /g, '') // clean color codes
+    .replaceAll(regex.description.colorcodes, '') // clean color codes
+    .replaceAll(regex.description.newlines, ' ') // clean new lines
     .trim() // get rid of trailing whitespaces
 }
 
